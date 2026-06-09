b43ea6f1294c50286da44d33639228210b97bcae
Himanshu-kulkarni <kulhimanshu2407@gmail.com>
2026-06-10 02:02:37 +0530

    feat: implement user registration endpoint

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -1,8 +1,34 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from app.schemas import UserCreate
+from app.models import User
+from app.database import get_db
+from app.utils.security import hash_password
 
 router = APIRouter()
 
+@router.post("/register")
+def register_user(
+    user: UserCreate,
+    db: Session = Depends(get_db)
+):
+    
+    hashed_password = hash_password(user.password)
 
-@router.get("/test")
-def test_route():
-    return {"message": "Auth Route Working"}
+    new_user = User(
+        name=user.name,
+        email=user.email,
+        password_hash=hashed_password
+    )
+
+    db.add(new_user)
+
+    db.commit()
+
+    db.refresh(new_user)
+
+    return {
+        "message": "User Registered Successfully",
+        "user_id": new_user.id
+    }
