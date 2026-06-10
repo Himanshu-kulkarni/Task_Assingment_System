f4e8bf9182ae16c93c9e8a062e8d389205065a4c
Himanshu-kulkarni <kulhimanshu2407@gmail.com>
2026-06-10 12:11:15 +0530

    feat: implement JWT authentication and protected routes

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -1,12 +1,17 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from app.schemas import UserCreate
+from app.schemas import UserCreate, UserLogin
 from app.models import User
 from app.database import get_db
-from app.utils.security import hash_password
+from app.utils.security import (
+    hash_password,
+    verify_password,
+    create_access_token
+)
 
 from fastapi import HTTPException
+from app.dependencies import get_current_user
 
 router = APIRouter()
 
@@ -44,3 +49,50 @@ def register_user(
         "message": "User Registered Successfully",
         "user_id": new_user.id
     }
+
+
+@router.post("/login")
+def login_user(
+    user: UserLogin,
+    db: Session = Depends(get_db)
+):
+    
+    existing_user = db.query(User).filter(
+        User.email == user.email
+    ).first()
+
+    if not existing_user:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid email or password"
+        )
+
+    is_valid = verify_password(
+        user.password,
+        existing_user.password_hash
+    )
+
+    if not is_valid:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid email or password"
+        )
+
+    access_token = create_access_token(
+    data={
+        "user_id": existing_user.id
+    }
+)
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
+
+
+@router.get("/me")
+def get_me(
+    current_user=Depends(get_current_user)
+):
+
+    return current_user
--- backend/app/utils/security.py
+++ backend/app/utils/security.py
@@ -1,4 +1,14 @@
 from passlib.context import CryptContext
+from jose import jwt
+from datetime import datetime, timedelta
+import os
+from dotenv import load_dotenv
+from jose import JWTError
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
 
 pwd_context = CryptContext(
     schemes=["bcrypt"],
@@ -7,3 +17,44 @@ pwd_context = CryptContext(
 
 def hash_password(password: str):
     return pwd_context.hash(password)
+
+def verify_password(
+    plain_password: str,
+    hashed_password: str
+):
+    return pwd_context.verify(
+        plain_password,
+        hashed_password
+    )
+
+def create_access_token(data: dict):
+
+    to_encode = data.copy()
+
+    expire = datetime.utcnow() + timedelta(hours=1)
+
+    to_encode.update({
+        "exp": expire
+    })
+
+    encoded_jwt = jwt.encode(
+        to_encode,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+
+    return encoded_jwt
+
+def verify_token(token: str):
+
+    try:
+        payload = jwt.decode(
+            token,
+            SECRET_KEY,
+            algorithms=[ALGORITHM]
+        )
+
+        return payload
+
+    except JWTError:
+        return None
