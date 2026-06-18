8c8df3b46bf99e38528c13622a2a7f0093fb7acd
Himanshu-kulkarni <kulhimanshu2407@gmail.com>
2026-06-18 17:58:46 +0530

    removed frontend files from backend

--- frontend/src/services/departmentService.js
+++
@@ -1,8 +0,0 @@
-import api from './api';
-
-export const departmentService = {
-  createDepartment: (data) => api.post('/departments', data),
-  getDepartments: () => api.get('/departments'),
-  getDepartmentMembers: (departmentId) => api.get(`/departments/${departmentId}/members`),
-  assignUser: (departmentId, userId) => api.post(`/departments/${departmentId}/assign-user/${userId}`),
-};
